2dadf9f101202ab8ca04157334793f0164f1465c
Josue Tchirktema <tchirktemajosue@gmail.com>
2018-12-18 13:40:47 +0000

    design index page

--- views/index.views.php
+++ views/index.views.php
@@ -7,8 +7,19 @@
   </head>
   <body>
 
+    <ul>
+      <?php if($_SESSION['logged_in']): ?>
+      <li style="float:right"> <a href="#"> <span class="profile"></span> </a> </li>
+      <li style="float:right"><a href="#about">Hi, <?php echo $_SESSION['prenom']; ?></a></li>
+      <li style="float:right"><a href="index.php?logout='true'">deconnexion</a></li>
+      <li style="float:right"><a href="#about">About</a></li>
+    <?php else: ?>
+      <li style="float:right"><a href="login.php">Connexion</a></li>
+      <li style="float:right"><a href="register.php">Inscription</a></li>
+      <li style="float:right"><a href="#about">About</a></li>
+    <?php endif ?>
+    </ul>
     <div class="container" id="header">
-      
       <form id="search" method="post">
         <div class="inner-form">
           <div class="input-field second-wrap">
