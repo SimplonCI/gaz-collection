92a286bac51f959518fc30e06a30c089b0d1404d
Josue Tchirktema <tchirktemajosue@gmail.com>
2018-12-20 11:13:14 +0000

    design du formulaire recherche

--- views/index.views.php
+++ views/index.views.php
@@ -2,6 +2,8 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/style.css">
     <title>E-GAZ</title>
   </head>
@@ -20,7 +22,40 @@
     <?php endif ?>
     </ul>
 
+
     <div class="container" id="header">
+      <form id="search" method="post">
+        <div class="inner-form">
+          <div class="input-field second-wrap">
+            <select id="commune" name="commune"/>
+              <option value="yopougon">Yopougon</option>
+              <option value="abobo">Abobo</option>
+            </select>
+          </div>
+          <div class="input-field second-wrap">
+            <select id="quartier" name="quartier"/>
+                <option value="">Niagon sub</option>
+                <option value="">Maroc</option>
+              </select>
+          </div>
+          <div class="input-field third-wrap">
+            <button class="btn-search" type="button">
+              <svg class="svg-inline--fa fa-search fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </form>
+
+    </div>
+
+
+
+
+
+
+    <!-- <div class="container" id="header">
       <form id="search" method="post">
         <div class="inner-form">
           <div class="input-field second-wrap">
@@ -34,21 +69,23 @@
             </button>
           </div>
         </div>
-      </form>
+      </form> -->
+
 
 
     </div>
 
+      <script src="assets/js/jquery.min.js" charset="utf-8"></script>
+    <!-- <script src="assets/select2/select2.js" charset="utf-8"></script> -->
 
-    <?php if(isset($_SESSION['logged_in'])): ?>
-
-       <img src="data:image/png;base64,<?php echo $_SESSION['profile']?>">
-       <?php $imageUrl = 'data:image/png;base64,'.$_SESSION['profile'] ?>
-       <script type="text/javascript">
-            document.getElementById('profile').style.background = "url('<?php echo $imageUrl?>')";
-       </script>
-
-    <?php endif ?>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+    <script type="text/javascript">
+          $(document).ready(function(){
+            $("#commune").select2();
+            $("#quartier").select2();
+          });
+          // var el = document.getElementById('quartier');
+          // el.style.width = '300px';
+    </script>
   </body>
 </html>
